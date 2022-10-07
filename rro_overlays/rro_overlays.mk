e2b82937db8941503198d41ac26e4af297f07da3
Jackeagle <jackeagle102@gmail.com>
2022-10-07 06:10:51 +0200

    rro_overlays: Import GMS and Pixel overlays
    Ref: https://gitlab.com/BlissLabs/BlissRoms/platform_vendor_gms/-/commits/t/common/proprietary/product/overlay
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I6e5ff38d21944d6028b95fe857ba34903c481743

--- rro_overlays/rro_overlays.mk
+++ rro_overlays/rro_overlays.mk
@@ -8,3 +8,35 @@ PRODUCT_PACKAGES += \
 # Custom Overlays
 #PRODUCT_PACKAGES += \
 #    CustomPixelLauncherOverlay
+
+# GMS Overlays
+ifeq ($(BLISS_BUILD_VARIANT), gapps)
+PRODUCT_PACKAGES += \
+    PixelBuiltInPrintService \
+    CaptivePortalLoginOverlay \
+    CellBroadcastReceiverOverlay \
+    CellBroadcastServiceOverlay \
+    PixelContactsProvider \
+    GoogleConfigOverlay \
+    GooglePermissionControllerOverlay \
+    GoogleWebViewOverlay \
+    ManagedProvisioningPixelOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlay2021 \
+    PixelConfigOverlayCommon \
+    PixelConnectivityOverlay2021 \
+    PixelSetupWizardOverlay \
+    PixelSetupWizardOverlay2019 \
+    PixelSetupWizardOverlay2021 \
+    PixelSetupWizard_rro \
+    PixelTetheringOverlay2021 \
+    PixelSettingsGoogle \
+    PixelSettingsProvider \
+    SystemUIGXOverlay \
+    PixelSystemUIGoogle \
+    PixelTeleService \
+    PixelTelecom \
+    Pixelframework-res \
+endif
