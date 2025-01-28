2b3c37c0505074a5f6eb4936d5d31c4b6c90e721
Jis G Jacob <studiokeys@blissroms.org>
2025-01-28 02:20:15 -0500

    overlays: Dump unused stuff
    Change-Id: I43fc9c054398852085ee6fd24964d213b2e43284
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_overlays.mk
+++ bliss_overlays.mk
@@ -20,56 +20,18 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/common
 
-# Gapps overlays
-#ifeq ($(BLISS_BUILD_VARIANT), gapps)
-#DEVICE_PACKAGE_OVERLAYS += \
-#    vendor/overlays/overlay/gapps
-
-#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/gapps
-#endif
-
-#ifeq ($(BLISS_BUILD_VARIANT), gapps)
-#ifneq ($(filter blueline bonito coral crosshatch sunfish taimen,$(BLISS_BUILD)),)
-#DEVICE_PACKAGE_OVERLAYS += \
-#    vendor/overlays/overlay/pixel
-#
-#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/pixel
-#endif
-#endif
-
-# QS Tile styles
-#include vendor/overlays/QSThemes/QSThemes.mk
-
-# Accents
-#include vendor/overlays/accents/accents.mk
-
 # System themes
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
 
-# Primary Colors
-#include vendor/overlays/primary/primary_colors.mk
-
 # Fonts
 include vendor/overlays/fonts/fonts.mk
-
-# Switch Themes
-#include vendor/overlays/switch/switches.mk
 
 # Statusbar Icons Themes
 include vendor/overlays/icons/icons.mk
 
 # Navbar styles
 include vendor/overlays/navbar/navbar.mk
-
-# Gvm styles
-#include vendor/overlays/Gvm/gvm.mk
-
-#Rounded Corner Styles
-#include vendor/overlays/rounded/rounded.mk
-
-# Cutout control overlays
-#include vendor/overlays/NotchCity/NotchCity.mk
 
 # RRO Overlays
 include vendor/overlays/rro_overlays/rro_overlays.mk
@@ -78,21 +40,3 @@ include vendor/overlays/rro_overlays/rro_overlays.mk
 ifeq ($(EXTRA_UDFPS_ANIMATIONS),true)
 include vendor/overlays/UDFPS/udfps.mk
 endif
-
-# Brightness slider styles
-PRODUCT_PACKAGES += \
-    BrightnessSliderFilledOverlay \
-    BrightnessSliderThinOverlay
-
-# QS UI Style
-PRODUCT_PACKAGES += \
-    A11QSUI \
-    QSOutline \
-    QSTwoToneAccent \
-    QSTwoToneAccentTrans \
-    QSShaded \
-    QSCyberPunk \
-    QSNeumorph \
-    QSReflected \
-    QSSurround \
-    QSThin
